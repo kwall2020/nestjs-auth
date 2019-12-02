9c31e19800f0a6c125d3cecc57e64eabe8a76c29
kwall2004 <kwall2004@gmail.com>
2019-12-02 09:29:32 -0500

    add jwt key

--- src/constants.ts
+++ src/constants.ts
@@ -1,3 +1,11 @@
 export const jwtConstants = {
-  secret: 'secretKey',
+  secret: '-----BEGIN PUBLIC KEY-----\n' +
+  'MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAlVoORdHAjdAKuI0FiIIR\n' +
+  'CyfLx0dCQ8tDsNPdOT6u9cnwVN1GDG00egYMIZWe7MAREZiYctbrenWSMI+IyyUK\n' +
+  'qGiW0+5RhxkPR1xNEJZZ360BwdrPg/5vxOJVZypitQsn/fMLgXTSOZdrXUVzoscD\n' +
+  'jMYJBnxEdwkfFDPkOe7zt0ST/f6IpR3JCIX1mP5/qg0vwhWQCjkVXgbeu/KNEeWk\n' +
+  'wmFiLNAiheQFtVx3jVBYcfkuvORPbzLH8TOJ14q5hbdgm+R5Efn/m5H3PbA3LDLe\n' +
+  'xn3JXYocBBRVj3gISBZX+R395jXeCAfHP6+wXb2zg/2dihhcBz+6Ln3thuzfcpZr\n' +
+  'ZwIDAQAB\n' +
+  '-----END PUBLIC KEY-----',
 };
